9794fcb336d8a1ff515e4be1687680ca526be1e4
Kenn CHONG <kenn.chong@sap.com>
2022-07-05 15:29:08 -0400

    amchart testing

---
+++ customwidgets/amChartTesting/amchart.js
@@ -0,0 +1,216 @@
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+	<!-- Styles -->
+	<style>
+	#chartdiv {
+	  width: 100%;
+	  height: 500px;
+	}
+	</style>
+	
+	<!-- Resources -->
+	<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+	<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+	<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+	
+	<!-- Chart code -->
+	<script>
+	am5.ready(function() {
+	
+	// Create root element
+	// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+	var root = am5.Root.new("chartdiv");
+	
+	
+	// Set themes
+	// https://www.amcharts.com/docs/v5/concepts/themes/
+	root.setThemes([
+	  am5themes_Animated.new(root)
+	]);
+	
+	
+	// Create chart
+	// https://www.amcharts.com/docs/v5/charts/xy-chart/
+	var chart = root.container.children.push(am5xy.XYChart.new(root, {
+	  panX: true,
+	  panY: true,
+	  wheelX: "panX",
+	  wheelY: "zoomX",
+	  pinchZoomX:true
+	}));
+	
+	// Add cursor
+	// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+	var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+	cursor.lineY.set("visible", false);
+	
+	
+	// Create axes
+	// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+	var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
+	xRenderer.labels.template.setAll({
+	  rotation: -90,
+	  centerY: am5.p50,
+	  centerX: am5.p100,
+	  paddingRight: 15
+	});
+	
+	var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+	  maxDeviation: 0.3,
+	  categoryField: "country",
+	  renderer: xRenderer,
+	  tooltip: am5.Tooltip.new(root, {})
+	}));
+	
+	var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+	  maxDeviation: 0.3,
+	  renderer: am5xy.AxisRendererY.new(root, {})
+	}));
+	
+	
+	// Create series
+	// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+	var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+	  name: "Series 1",
+	  xAxis: xAxis,
+	  yAxis: yAxis,
+	  valueYField: "value",
+	  sequencedInterpolation: true,
+	  categoryXField: "country",
+	  tooltip: am5.Tooltip.new(root, {
+		labelText:"{valueY}"
+	  })
+	}));
+	
+	series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
+	series.columns.template.adapters.add("fill", function(fill, target) {
+	  return chart.get("colors").getIndex(series.columns.indexOf(target));
+	});
+	
+	series.columns.template.adapters.add("stroke", function(stroke, target) {
+	  return chart.get("colors").getIndex(series.columns.indexOf(target));
+	});
+	
+	
+	// Set data
+	var data = [{
+	  country: "USA",
+	  value: 2025
+	}, {
+	  country: "China",
+	  value: 1882
+	}, {
+	  country: "Japan",
+	  value: 1809
+	}, {
+	  country: "Germany",
+	  value: 1322
+	}, {
+	  country: "UK",
+	  value: 1122
+	}, {
+	  country: "France",
+	  value: 1114
+	}, {
+	  country: "India",
+	  value: 984
+	}, {
+	  country: "Spain",
+	  value: 711
+	}, {
+	  country: "Netherlands",
+	  value: 665
+	}, {
+	  country: "Russia",
+	  value: 580
+	}, {
+	  country: "South Korea",
+	  value: 443
+	}, {
+	  country: "Canada",
+	  value: 441
+	}];
+	
+	xAxis.data.setAll(data);
+	series.data.setAll(data);
+	
+	
+	// Make stuff animate on load
+	// https://www.amcharts.com/docs/v5/concepts/animations/
+	series.appear(1000);
+	chart.appear(1000, 100);
+	
+	}); // end am5.ready()
+	</script>
+	
+	<!-- HTML -->
+	<div id="chartdiv"></div>
+	`;
+
+	class Box extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			
+			this.$style = shadowRoot.querySelector('style');			
+			this.$svg = shadowRoot.querySelector('svg');
+			
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			
+			this._props = {};
+		}
+		
+		render(val, info, color) {
+			var val1 = val * 0.01;
+			var x = this.svg_circle_arc_path(500, 500, 450, -90, val1 * 180.0 - 90);
+			var rounded = Math.round( val * 10 ) / 10;
+
+			
+			if(rounded >=0 && rounded <=100) {
+				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
+				this.$svg.innerHTML = '<path d="M 950 500 A 450 450 0 0 0 50 500"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
+			}
+		}
+		  
+		polar_to_cartesian(cx, cy, radius, angle) {
+		    var radians;
+		    radians = (angle - 90) * Math.PI / 180.0;
+		    return [Math.round((cx + radius * Math.cos(radians)) * 100) / 100, Math.round((cy + radius * Math.sin(radians)) * 100) / 100];
+		}
+		
+		svg_circle_arc_path(x, y, radius, start_angle, end_angle) {
+		    var end_xy, start_xy;
+		    start_xy = this.polar_to_cartesian(x, y, radius, end_angle);
+		    end_xy = this.polar_to_cartesian(x, y, radius, start_angle);
+		    return "M " + start_xy[0] + " " + start_xy[1] + " A " + radius + " " + radius + " 0 0 0 " + end_xy[0] + " " + end_xy[1];
+		  };
+		  
+
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("value" in changedProperties) {
+				this.$value = changedProperties["value"];
+			}
+			
+			if ("info" in changedProperties) {
+				this.$info = changedProperties["info"];
+			}
+			
+			if ("color" in changedProperties) {
+				this.$color = changedProperties["color"];
+			}
+			
+			this.render(this.$value, this.$info, this.$color);
+		}
+	}
+	
+	customElements.define("com-demo-gauge", Box);
+})();
