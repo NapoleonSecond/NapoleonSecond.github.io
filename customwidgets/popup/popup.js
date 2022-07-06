c20c5930b1979601a0d4ac370068bc18b7957bda
Kenn CHONG <kenn.chong@sap.com>
2022-07-06 15:44:33 -0400

    tried to shift different values around

--- customwidgets/popup/popup.js
+++ customwidgets/popup/popup.js
@@ -15,43 +15,43 @@
     class PopUpInfo extends HTMLElement {
         constructor() {
           // Always call super first in constructor
-          super();
+            super();
       
           // Create a shadow root
-          const shadow = this.attachShadow({mode: 'open'});
+            const shadow = this.attachShadow({mode: 'open'});
       
           // Create spans
-          const wrapper = document.createElement('span');
-          wrapper.setAttribute('class', 'wrapper');
+            const wrapper = document.createElement('span');
+            wrapper.setAttribute('class', 'wrapper');
       
-          const icon = document.createElement('span');
-          icon.setAttribute('class', 'icon');
-          icon.setAttribute('tabindex', 0);
+            const icon = document.createElement('span');
+            icon.setAttribute('class', 'icon');
+            icon.setAttribute('tabindex', 0);
       
-          const info = document.createElement('span');
-          info.setAttribute('class', 'info');
+            const info = document.createElement('span');
+            info.setAttribute('class', 'info');
       
           // Take attribute content and put it inside the info span
-          const text = this.getAttribute('data-text');
-          info.textContent = text;
+            const text = this.getAttribute('data-text');
+            info.textContent = text;
       
           // Insert icon
-          let imgUrl;
-          if(this.hasAttribute('img')) {
-            imgUrl = this.getAttribute('img');
-          } else {
-            imgUrl = 'img/default.png';
+            let imgUrl;
+            if(this.hasAttribute('img')) {
+                imgUrl = this.getAttribute('img');
+            } else {
+                imgUrl = 'img/default.png';
           }
       
-          const img = document.createElement('img');
-          img.src = imgUrl;
-          icon.appendChild(img);
+            const img = document.createElement('img');
+            img.src = imgUrl;
+            icon.appendChild(img);
       
           // Create some CSS to apply to the shadow dom
-          const style = document.createElement('style');
-          console.log(style.isConnected);
+            const style = document.createElement('style');
+            console.log(style.isConnected);
       
-          style.textContent = `
+            style.textContent = `
             .wrapper {
               position: relative;
             }
@@ -82,11 +82,17 @@
           `;
       
           // Attach the created elements to the shadow dom
-          shadow.appendChild(style);
-          console.log(style.isConnected);
-          shadow.appendChild(wrapper);
-          wrapper.appendChild(icon);
-          wrapper.appendChild(info);
+            shadow.appendChild(style);
+            console.log(style.isConnected);
+            shadow.appendChild(wrapper);
+            wrapper.appendChild(icon);
+            wrapper.appendChild(info);
+
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.dispatchEvent(event);
+            });
+            this._props = {};
         }
 
         onCustomWidgetBeforeUpdate(changedProperties) {
