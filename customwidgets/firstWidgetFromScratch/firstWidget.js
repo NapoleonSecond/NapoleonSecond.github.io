f37bf94320430fc1b4520c517e7dc64e282951e8
Kenn CHONG <kenn.chong@sap.com>
2022-07-08 15:13:56 -0400

    Trying to Alter the code

--- customwidgets/firstWidgetFromScratch/firstWidget.js
+++ customwidgets/firstWidgetFromScratch/firstWidget.js
@@ -3,23 +3,23 @@
     tmpl.innerHTML = `
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-sap-sample-helloworld4', class HelloWorld extends HTMLElement {
 
 
 		constructor() {
 			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
             this._tagContainer;
-            this._tagType="h1";
-            this._tagText="Hello World";
+            this._tagType = "h1";
+            this._tagText = "Hello World";
 		}
-
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this._firstConnection = true;
-            this.redraw();
+            this.redraw(); 
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -31,46 +31,59 @@
 		onCustomWidgetBeforeUpdate(oChangedProperties) {
 
 		}
-        
+
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            
             if (this._firstConnection){
                 this.redraw();
             }
-            
-        }
-        get widgetText() {
-            return this._tagType;
-        }
-        
-        set widgetText(value) {
-            this._tagText = value;
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+        
         }
 
+        
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // Commented out by default
         /*
         onCustomWidgetResize(width, height){
         
         }
         */
 
-        redraw(){
-            if(this._tagText != null){
-                if (this._tagContainer){
-                    this._tagContainer.parentNode.removeChild(this._tagContainer);
-                }
+        //Getters and Setters
+        get widgetText() {
+            return this._tagType;
+        }
 
-                var shadow = window.getSelection(this._shadowroot);
-                this._tagContainer = document.createElement(this._type);
-                
+        set widgetText(value) {
+            this._tagText = value;
+        }
+
+
+        get headingType() {
+            return this._tagType;
             }
+
+        set headingType(value) {
+            this._tagType = value;
+        }
+
+        // End - Getters and Setters
+
+        redraw(){
+            if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
+            }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tagText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
+
         }
     
     
