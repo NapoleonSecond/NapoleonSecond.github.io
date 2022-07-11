071b9afa192a3359d7ec7b520bf24a103bd3faed
Kenn CHONG <kenn.chong@sap.com>
2022-07-11 08:53:08 -0400

    EchartsTesting

---
+++ customwidgets/EChartsTesting/main.js
@@ -0,0 +1,59 @@
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+  
+  (function () {
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `
+    class SamplePrepared extends HTMLElement {
+      constructor () {
+        super()
+  
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+  
+        this._root = this._shadowRoot.getElementById('root')
+  
+        this._props = {}
+  
+        this.render()
+      }
+  
+      onCustomWidgetResize (width, height) {
+        this.render()
+      }
+  
+      async render () {
+        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+  
+        const chart = echarts.init(this._root)
+        const option = {
+          // https://echarts.apache.org/examples/zh/index.html
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              data: [820, 932, 901, 934, 1290, 1330, 1320],
+              type: 'line',
+              smooth: true
+            }
+          ]
+        }
+        chart.setOption(option)
+      }
+    }
+  
+    customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+  })()
