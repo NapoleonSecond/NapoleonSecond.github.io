0d7044c0496b4208edde626ee8830b824d2bdc63
Kenn CHONG <kenn.chong@sap.com>
2022-07-15 16:07:24 -0400

    added simple pie

--- customwidgets/EChartSimpleBar/EChartSimpleBar.js
+++ customwidgets/EChartSimpleBar/EChartSimpleBar.js
@@ -12,7 +12,7 @@ var getScriptPromisify = (src) => {
         <div id="root" style="width: 100%; height: 100%;">
         </div>
       `
-    class SamplePrepared extends HTMLElement {
+    class simplePie extends HTMLElement {
       constructor () {
         super()
   
@@ -104,5 +104,5 @@ var getScriptPromisify = (src) => {
       }
     }
   
-    customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+    customElements.define('com-sap-sample-echarts-simple-pie', simplePie)
   })()
