796727a20f69f7bff6f5eb3192f91aa0ee096eee
Kenn CHONG <kenn.chong@sap.com>
2022-07-26 15:42:21 -0400

    added real google calendar

---
+++ customwidgets/googleCalendar/googleCalendar.js
@@ -0,0 +1,107 @@
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+  
+  (function () {
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        <div id="chart_div" style="width: 900px; height: 500px;"></div>
+        </div>
+      `
+    class calendar extends HTMLElement {
+      constructor () {
+        super();
+        this.test = "just a testing string";
+        this._test = "just another testing stirng";
+
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+
+        this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+  
+        this._root = this._shadowRoot.getElementById('root');
+        this._chart = this._shadowRoot.getElementById('chart_div');
+  
+        this._props = {};
+  
+        this.render();
+      }
+
+  
+      onCustomWidgetResize (width, height) {
+        this.render()
+      }
+  
+      async render () {
+        await getScriptPromisify('https://www.gstatic.com/charts/loader.js')
+
+        google.charts.load("current", {packages:["calendar"]});
+        google.charts.setOnLoadCallback(this.drawChart);
+        
+        
+      }
+
+      drawChart = () => {
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({ type: 'date', id: 'Date' });
+        dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
+        dataTable.addRows([
+          [ new Date(2012, 3, 13), 37032 ],
+          [ new Date(2012, 3, 14), 38024 ],
+          [ new Date(2012, 3, 15), 38024 ],
+          [ new Date(2012, 3, 16), 38108 ],
+          [ new Date(2012, 3, 17), 38229 ],
+          // Many rows omitted for brevity.
+          [ new Date(2013, 9, 4), 38177 ],
+          [ new Date(2013, 9, 5), 38705 ],
+          [ new Date(2013, 9, 12), 38210 ],
+          [ new Date(2013, 9, 13), 38029 ],
+          [ new Date(2013, 9, 19), 38823 ],
+          [ new Date(2013, 9, 23), 38345 ],
+          [ new Date(2013, 9, 24), 38436 ],
+          [ new Date(2013, 9, 30), 38447 ]
+        ]);
+
+       var chart = new google.visualization.Calendar(this._shadowRoot.getElementById('chart_div'));
+
+       var options = {
+         title: "Red Sox Attendance",
+         height: 350,
+       };
+
+       chart.draw(dataTable, options);
+   
+      }
+
+      drawVisualization = () => {
+        console.log(this.test);
+        console.log(this._test);
+        // Some raw data (not necessarily accurate)
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+          ['2004/05',  165,      938,         522,             998,           450,      614.6],
+          ['2005/06',  135,      1120,        599,             1268,          288,      682],
+          ['2006/07',  157,      1167,        587,             807,           397,      623],
+          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+        ]);
+      
+        var options = {
+          title : 'Monthly Coffee Production by Country',
+          vAxis: {title: 'Cups'},
+          hAxis: {title: 'Month'},
+          seriesType: 'bars',
+          series: {5: {type: 'line'}}
+        };
+        
+        var chart = new google.visualization.ComboChart(this._chart);
+        chart.draw(data, options);
+      }
+    }
+  
+    customElements.define('com-sap-sample-simple-calendar', calendar)
+  })()
