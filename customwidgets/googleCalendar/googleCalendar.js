a5a4a85abe5f1339fd9b27f2b3d1ea29ff4df7f3
Kenn CHONG <kenn.chong@sap.com>
2022-08-01 12:57:41 -0400

    Update googleCalendar.js

--- customwidgets/googleCalendar/googleCalendar.js
+++ customwidgets/googleCalendar/googleCalendar.js
@@ -50,38 +50,7 @@ var getScriptPromisify = (src) => {
         
       }
 
-      drawChart = () => {
-        var dataTable = new google.visualization.DataTable();
-        console.log(this.test);
-        dataTable.addColumn({ type: 'date', id: 'Date' });
-        dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
-        dataTable.addRows([
-          [ new Date(2012, 3, 13), 37032 ],
-          [ new Date(2012, 3, 14), 38024 ],
-          [ new Date(2012, 3, 15), 38024 ],
-          [ new Date(2012, 3, 16), 38108 ],
-          [ new Date(2012, 3, 17), 38229 ],
-          // Many rows omitted for brevity.
-          [ new Date(2013, 9, 4), 38177 ],
-          [ new Date(2013, 9, 5), 38705 ],
-          [ new Date(2013, 9, 12), 38210 ],
-          [ new Date(2013, 9, 13), 38029 ],
-          [ new Date(2013, 9, 19), 38823 ],
-          [ new Date(2013, 9, 23), 38345 ],
-          [ new Date(2013, 9, 24), 38436 ],
-          [ new Date(2013, 9, 30), 38447 ]
-        ]);
-
-       var chart = new google.visualization.Calendar(this._shadowRoot.getElementById('chart_div'));
-
-       var options = {
-         title: "Red Sox Attendance",
-         height: 350,
-       };
-
-       chart.draw(dataTable, options);
-   
-      }
+    
 
     }
   
