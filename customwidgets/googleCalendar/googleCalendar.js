8a02177cffed2afa2d2ec90cd2c31b96dee6657e
Kenn CHONG <kenn.chong@sap.com>
2022-08-01 13:08:56 -0400

    Update googleCalendar.js

--- customwidgets/googleCalendar/googleCalendar.js
+++ customwidgets/googleCalendar/googleCalendar.js
@@ -16,7 +16,7 @@ var getScriptPromisify = (src) => {
     class Googlecalendar extends HTMLElement {
       constructor () {
         super();
-        this.test = "just a testing string for google calendar";
+        this.test = "just a testing string for google";
 
         this._shadowRoot = this.attachShadow({ mode: 'open' });
 
@@ -41,14 +41,40 @@ var getScriptPromisify = (src) => {
         this.myDataBinding.data.forEach(element => {
           console.log(element); 
       });
-        
-
         google.charts.load("current", {packages:["calendar"]});
         google.charts.setOnLoadCallback(this.drawChart);
-
-     
-        
       }
+      drawChart = ()=> {
+        console.log(this.test);
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({ type: 'date', id: 'Date' });
+        dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
+        dataTable.addRows([
+           [ new Date(2012, 3, 13), 37032 ],
+           [ new Date(2012, 3, 14), 38024 ],
+           [ new Date(2012, 3, 15), 38024 ],
+           [ new Date(2012, 3, 16), 38108 ],
+           [ new Date(2012, 3, 17), 38229 ],
+           // Many rows omitted for brevity.
+           [ new Date(2013, 9, 4), 38177 ],
+           [ new Date(2013, 9, 5), 38705 ],
+           [ new Date(2013, 9, 12), 38210 ],
+           [ new Date(2013, 9, 13), 38029 ],
+           [ new Date(2013, 9, 19), 38823 ],
+           [ new Date(2013, 9, 23), 38345 ],
+           [ new Date(2013, 9, 24), 38436 ],
+           [ new Date(2013, 9, 30), 38447 ]
+         ]);
+ 
+        var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+ 
+        var options = {
+          title: "Red Sox Attendance",
+          height: 350,
+        };
+ 
+        chart.draw(dataTable, options);
+    }
 
     
 
