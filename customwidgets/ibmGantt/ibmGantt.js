497191b6c8e3cf52820aa67a045ef7fa672ff7b8
Chong <kenn.chong@sap.com>
2023-01-09 02:12:38 -0500

    new ibm

--- customwidgets/ibmGantt/ibmGantt.js
+++ customwidgets/ibmGantt/ibmGantt.js
@@ -131,7 +131,7 @@ var getScriptPromisify = (src) => {
 
 
         
-        var data = [
+        var data2 = [
           {
             id: 'NURSES+Anne',
             name: 'Anne',
@@ -227,7 +227,7 @@ var getScriptPromisify = (src) => {
           data: {
             // Configures how to fetch resources for the Gantt
             resources: {
-              data: data, // resources are provided in an array. Instead, we could configure a request to the server.
+              data: data2, // resources are provided in an array. Instead, we could configure a request to the server.
               // Activities of the resources are provided along with the 'activities' property of resource objects.
               // Alternatively, they could be listed from the 'data.activities' configuration.
               activities: 'activities',
