c6955cfec434a94f48c05e7a1e9cc1d353a6c732
Chong <kenn.chong@sap.com>
2023-01-08 23:54:11 -0500

    new ibm

--- customwidgets/ibmGantt/ibmGantt.js
+++ customwidgets/ibmGantt/ibmGantt.js
@@ -102,12 +102,14 @@ var getScriptPromisify = (src) => {
             
             var valid = originalEndTime != "@NullMember" && originalStartTime != "@NullMember" && this.data[index]["START_DATE"] != "@NullMember" && this.data[index]["END_DATE"] != "@NullMember" && this.data[index]["END_DATE"] != " " && this.data[index]["END_DATE"] != "";
 
+            /*
             if(valid){
               console.log(startDate.toISOString());
               console.log(this.data[index]["END_DATE"]);
               console.log(originalEndTime);
               console.log(endDate.toISOString())
             }
+            */
             if(originalStartTime != NaN && this.data[index]["VARIANTE"] != ""){
               if(valid){
                 dataVals.push(
