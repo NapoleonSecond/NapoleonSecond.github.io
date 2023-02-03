99b8f51acac313e9af16a42ed7e7855395c4c65e
Chong <kenn.chong@sap.com>
2023-02-03 09:59:41 -0500

    adding slight changes

--- customwidgets/adaptableGantt/ibmGanttAdaptable.js
+++ customwidgets/adaptableGantt/ibmGanttAdaptable.js
@@ -146,7 +146,7 @@ var getScriptPromisify = (src) => {
             } 
           }
           console.log(catToDate);
-          console.log(catToDate["INEED"]);
+          console.log(catToDate.get("INEED"));
           console.log(dataVals);
           
           var config = {
